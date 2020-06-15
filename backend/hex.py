0e1d72d9639efdc629676355538ef647a071acfa
joshuamundinger <josh.mundinger@gmail.com>
2020-06-14 23:38:31 -0500

    hex init and aura toggle

--- backend/hex.py
+++ backend/hex.py
@@ -2,4 +2,22 @@
 hex represents one of 28 hexes on the Piously board. 
 """
 
+class Hex(object):
+    def __init__(self, _location, _room):
+        self.location = _location
+        self.room = _room
+        self.aura = None
+
+    def toggle_aura():
+        if self.aura == None
+            self.aura = "Dark"
+        else if self.aura == "Dark"
+            self.aura = "Light"
+        else if self.aura == "Light"
+            self.aura = None
+        else:
+            raise RuntimeError "An aura was mis-set."
+    
+
+
 
