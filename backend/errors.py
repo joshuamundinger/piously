5c0f8a1dc22af24c2ee5baf733a23c8931ae5f94
Rachel Diamond <rachelsdiamond@gmail.com>
2020-06-15 15:00:15 -0500

    add InvalidMove error

---
+++ backend/errors.py
@@ -0,0 +1,13 @@
+"""
+Game specific error types
+"""
+
+class InvalidMove(Exception, object):
+    """Raised when an invalid move is attempted"""
+    pass
+
+    def __init__(self, error_message):
+        self.full_error = 'INVALID MOVE: {}'.format(error_message)
+
+    def __str__(self):
+        return self.full_error
