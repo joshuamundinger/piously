251dd9fa303364722f12815dd600445dd42de2df
Rachel Diamond <rachelsdiamond@gmail.com>
2020-06-18 00:20:19 -0500

    fix Artwork.__str__, Operation, spacing

--- backend/artwork.py
+++ backend/artwork.py
@@ -9,8 +9,12 @@ class Artwork(object):
         self.faction = None
 
     def __str__(self):
+        location = 'Not placed'
+        if self.hex != None:
+            location = self.hex.location
+        
         return "{color}\t({faction}:{location})".format(
             color = self.color,
             faction = self.faction or 'No faction',
-            location = self.hex and self.hex.location or 'Not placed'
+            location = location,
         )
